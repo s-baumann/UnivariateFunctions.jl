b44d5e073976d765c25f085518ddca5ae60216cb
Stuart Baumann <stuart@stuartbaumann.com>
2021-09-27 23:33:42 +0100

    Fixing Documentation Error

--- src/date_conversions.jl
+++ src/date_conversions.jl
@@ -20,7 +20,7 @@ end
 
 """
     years_from_global_base(a::Union{DateTime,Date})
-The number of years (calculated by the years_between function) between a given date
+The number of years (calculated by the `years_between` function) between a given date
 and the 1st of January 2000.
 
 ### Inputs
